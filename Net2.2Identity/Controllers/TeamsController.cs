0b6761dac53f794c2dd5fdab40b1e85c257cfdc2
Elijah Adewuyi <Eadewuyi@wragbysolutions.com>
2019-09-30 15:58:15 +0100

    team

--- Net2.2Identity/Controllers/TeamsController.cs
+++ Net2.2Identity/Controllers/TeamsController.cs
@@ -2,21 +2,80 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Net2._2Identity.Data;
+using Net2._2Identity.Models;
+using TME.Models;
 
 namespace TME.Controllers
 {
+  [Authorize]
     public class TeamsController : Controller
     {
-        public IActionResult Index()
+
+    private readonly ApplicationDbContext _context;
+    private readonly UserManager<ApplicationUser> _userManager;
+
+    public TeamsController(ApplicationDbContext context, UserManager<ApplicationUser> userManager)
+    {
+      _context = context;
+      _userManager = userManager;
+
+    }
+
+    public IActionResult Index()
         {
-            return View();
+      var teams = _context.Teams.ToList();
+            return View(teams);
         }
 
     public IActionResult TeamInfo()
     {
       return View();
     }
+
+    [HttpPost]
+    public IActionResult AddTeam([FromBody]Team team)
+    {
+      if (team == null)
+      {
+        return Json(new
+        {
+          msg = "No Data"
+        }
+       );
+      }
+
+      team.Id = Guid.NewGuid();
+      team.IsActive = true;
+      //mentor.
+
+      try
+      {
+
+        _context.Add(team);
+        _context.SaveChanges();
+
+        return Json(new
+        {
+          msg = "Success"
+        }
+  );
+      }
+      catch (Exception ee)
+      {
+
+      }
+
+      return Json(
+      new
+      {
+        msg = "Fail"
+      });
+    }
+
 
   }
 }
